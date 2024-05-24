79d634fd8d4741834791277c30e7e57a63596e7a
pabloruizaltuzara <pabloaltuzara@gmail.com>
2024-05-24 07:17:34 -0400

    images adds

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MenuItem{
+class MenuItem {
   final String title;
   final String subTitle;
   final String link;
   final IconData icon;
 
-  const MenuItem({
-    required this.title,
-    required this.subTitle,
-    required this.link,
-    required this.icon
-  });
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
 }
 
 const appMenuItems = <MenuItem>[
-  MenuItem(title: 'Botones', subTitle: 'Varios botones', link: '/buttons', icon: Icons.smart_button_outlined),
-  MenuItem(title: 'Tarjetas', subTitle: 'Contenedor estilizado', link: '/cards', icon: Icons.credit_card),
-  MenuItem(title: 'Progres Indicators', subTitle: 'generales y controlados', link: '/progress', icon: Icons.refresh_rounded),
-  MenuItem(title: 'Snackbars', subTitle: 'indicadores en pantalla', link: '/snackbars', icon: Icons.info_outline),
-  MenuItem(title: 'Animated container', subTitle: 'Stateful widget animado', link: '/animated', icon: Icons.check_box_outlined),
-  MenuItem(title: 'UI controls + tiles', subTitle: 'Una serie de controles', link: '/ui-controls', icon: Icons.car_rental_outlined),
-
+  MenuItem(
+      title: 'Botones',
+      subTitle: 'Varios botones',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
+  MenuItem(
+      title: 'Tarjetas',
+      subTitle: 'Contenedor estilizado',
+      link: '/cards',
+      icon: Icons.credit_card),
+  MenuItem(
+      title: 'Progres Indicators',
+      subTitle: 'generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'Snackbars',
+      subTitle: 'indicadores en pantalla',
+      link: '/snackbars',
+      icon: Icons.info_outline),
+  MenuItem(
+      title: 'Animated container',
+      subTitle: 'Stateful widget animado',
+      link: '/animated',
+      icon: Icons.check_box_outlined),
+  MenuItem(
+      title: 'UI controls + tiles',
+      subTitle: 'Una serie de controles',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introduccion a la aplicacion',
+      subTitle: 'Pequeno tutorial intro',
+      link: '/tutorial',
+      icon: Icons.accessibility_rounded),
 ];
